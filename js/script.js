ad3de2d2b5fb71e32baab70d0fe6ab0017e24faf
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-02-27 18:23:47 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Defining variable myName and then asking the browser to print in on the screen
+let myName = 'Jose'; 
+document.write(myName);
+/* Another way of adding a comment in a js file. Here, the code below is changing
+the value for myName and then asking the browser to print that new value. */
+myName = 'Zae'; 
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize *2) - (size / 2);
+document.write(minSize);
