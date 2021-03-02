b93431eaa33e589376aaf4d6b828bd185e345657
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-01 23:34:23 -0800

    correct syntax for pokemon objects in pokemonList

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 let pokemonList = [
     {
-        name = 'Pikachu',
-        height = .4,
-        type = ['electric']
+        name: 'Pikachu',
+        height: .4,
+        type: ['electric']
     },
     {
-        name = 'Charmander',
-        height = .6,
-        type = ['fire']
+        name: 'Charmander',
+        height: .6,
+        type: ['fire']
     },
     {
-        name = 'Bulbasaur',
-        height = .7,
-        type = ['grass', 'poison']
+        name: 'Bulbasaur',
+        height: .7,
+        type: ['grass', 'poison']
     },
     {
-        name = 'Onix',
-        height = 8.8,
-        type = ['rock', 'ground']
+        name: 'Onix',
+        height: 8.8,
+        type: ['rock', 'ground']
     }
 ];
