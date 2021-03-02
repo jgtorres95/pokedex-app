47c3f8503c1f8e64860704c8d3d4023ae91344ee
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-01 23:36:20 -0800

    create 'for' loop that iterates over pokemonList
    Loop contains conditional that checks if pokemonList.height exceeds 1. If so, a special text is written. Otherwise a standard text is written.

--- js/script.js
+++ js/script.js
@@ -20,3 +20,14 @@ let pokemonList = [
         type: ['rock', 'ground']
     }
 ];
+
+// For loop that iterates over pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+    //Conditional that checks if the height of pokemonList[i] is greater than 1
+    if (pokemonList[i].height > 1) {
+        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!</p>");
+    //Otherwise do this    
+    } else {
+        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>");  
+    }
+}
