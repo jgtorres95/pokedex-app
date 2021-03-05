1b752d35bb9b0e9d01f41fc0d041292fa22469ce
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-04 23:29:03 -0800

    remove "for" loop that iterates over pokemonList

--- js/script.js
+++ js/script.js
@@ -21,12 +21,6 @@ let pokemonList = [
     }
 ];
 
-// For loop that iterates over pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-    //Conditional that checks if the height of pokemonList[i] is greater than 1
-    if (pokemonList[i].height > 1) {
-        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!</p>");
-    //Otherwise do this    
     } else {
         document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>");  
     }
