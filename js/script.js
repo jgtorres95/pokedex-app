1c74531791e01cea7fccc06428421a0da3a20767
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-04 23:35:20 -0800

    cut pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-let pokemonList = [
-    {
-        name: 'Pikachu',
-        height: .4,
-        type: ['electric']
-    },
-    {
-        name: 'Charmander',
-        height: .6,
-        type: ['fire']
-    },
-    {
-        name: 'Bulbasaur',
-        height: .7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Onix',
-        height: 8.8,
-        type: ['rock', 'ground']
     }
 ];
 
