86497a06f8a8c92493e020bbe2c4e06d3c20877f
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-04 23:38:08 -0800

    paste pokemonList into pokemonRepository

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 //Creating new pokemonRepository variable to hold pokemonList array within an IIFE
 let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Pikachu',
+            height: .4,
+            type: ['electric']
+        },
+        {
+            name: 'Charmander',
+            height: .6,
+            type: ['fire']
+        },
+        {
+            name: 'Bulbasaur',
+            height: .7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Onix',
+            height: 8.8,
+            type: ['rock', 'ground']
+        }
+    ];
+    
     }
 ];
 
