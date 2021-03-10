3af22b457e5e60e5c2e2fc99b3fced74ad91b043
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-03-09 23:40:36 -0800

    add showDetails to list of returns

--- js/script.js
+++ js/script.js
@@ -48,7 +48,9 @@ let pokemonRepository = (function(){
 
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem,
+        showDetails: showDetails
     }
 }())
 
